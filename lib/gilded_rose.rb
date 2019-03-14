ce89831280432331c1d18e9e2f082f499c3ba893
RubyKobayashi <ruby@rubykobayashi.com>
2019-03-14 16:14:47 +0000

    further refactoring: before_sell_in, constants and normal_products

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,24 +1,23 @@
 class GildedRose
 
+  MAXIMUM_QUALITY = 50
+  MINIMUM_QUALITY = 0
+
   def initialize(items)
     @items = items
+    @maximum_quality = 50
+    @minimum_quality = 0
   end
 
   def update_quality()
     @items.each do |item|
-      sulfuras(item)
-        backstage(item)
-        aged_brie(item)
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      end
+      update_sulfuras(item)
+      update_backstage(item)
+      update_aged_brie(item)
+      update_normal_products(item)
 
       # after SellIn
-      if item.sell_in < 0
+      if after_sell_in?(item)
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
@@ -31,43 +30,72 @@ class GildedRose
       end
     end
   end
+end
+
 
   private
 
-  def sulfuras(item)
-      item.name == "Sulfuras, Hand of Ragnaros"
+  def update_sulfuras(item)
+      sulfuras?(item)
       item.sell_in = item.sell_in - 1
     end
 
-    def aged_brie(item)
-      if item.name == "Aged Brie" && item.quality < 50
-      item.quality = item.quality + 1
+  def sulfuras?(item)
+    item.name == "Sulfuras, Hand of Ragnaros"
+  end
+
+  def update_aged_brie(item)
+    if aged_brie?(item) && quality_under_50?(item)
+    item.quality = item.quality + 1
     end
   end
 
-    def backstage(item)
-        if backstage_item?(item) && item.sell_in < 6 && item.quality < 50
-          item.quality = item.quality + 3 #3
-        elsif backstage_item?(item) && item.sell_in < 11 && item.quality < 50
-          item.quality = item.quality + 2
-        elsif backstage_item?(item) && item.sell_in < 0
-          item.quality = 0 #2
-      end
-        end
-
-        def backstage_item?(item)
-          item.name == "Backstage passes to a TAFKAL80ETC concert"
-        end
-
-        def after_sell_in?(item)
-          item.sell_in < 0
-        end
-
-
-
-
-
+  def aged_brie?(item)
+    item.name == "Aged Brie"
   end
+
+  def quality_under_50?(item)
+   item.quality < @maximum_quality
+  end
+
+  def update_backstage(item)
+    quality_under_50?(item)
+    if backstage_item?(item) && after_sell_in?(item)
+      item.quality = 0
+    elsif backstage_item?(item) && sell_in_5_days_or_less(item)
+      item.quality = item.quality + 3 #3
+    elsif backstage_item?(item) && sell_in_10_days_or_less(item)
+      item.quality = item.quality + 2
+    end
+  end
+
+  def backstage_item?(item)
+    item.name == "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def sell_in_10_days_or_less(item)
+    item.sell_in < 11
+  end
+
+  def sell_in_5_days_or_less(item)
+    item.sell_in < 6
+  end
+
+  def after_sell_in?(item)
+    item.sell_in < 0
+  end
+
+  def update_normal_products(item)
+    if !backstage_item?(item) && !aged_brie?(item) && !sulfuras?(item)
+      if item.quality > @minimum_quality
+        item.quality = item.quality - 1
+  end
+end
+
+end
+
+
+
 
 
 
