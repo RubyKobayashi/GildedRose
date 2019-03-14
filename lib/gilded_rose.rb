c9c2f31c06da5d3f750bdf943b3b96b2393ab5c9
RubyKobayashi <ruby@rubykobayashi.com>
2019-03-14 15:09:21 +0000

    extract 'brie' and 'backstage'

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,33 +6,18 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+      sulfuras(item)
+        backstage(item)
+        aged_brie(item)
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1
           end
         end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
       end
-      sulfuras(item)
-      # if item.name != "Sulfuras, Hand of Ragnaros"
-      #   item.sell_in = item.sell_in - 1
-      # end
+
+      # after SellIn
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
@@ -41,12 +26,6 @@ class GildedRose
                 item.quality = item.quality - 1
               end
             end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
           end
         end
       end
@@ -55,10 +34,39 @@ class GildedRose
 
   private
 
-  def sulfuras(item) 
+  def sulfuras(item)
       item.name == "Sulfuras, Hand of Ragnaros"
       item.sell_in = item.sell_in - 1
     end
+
+    def aged_brie(item)
+      if item.name == "Aged Brie" && item.quality < 50
+      item.quality = item.quality + 1
+    end
+  end
+
+    def backstage(item)
+        if backstage_item?(item) && item.sell_in < 6 && item.quality < 50
+          item.quality = item.quality + 3 #3
+        elsif backstage_item?(item) && item.sell_in < 11 && item.quality < 50
+          item.quality = item.quality + 2
+        elsif backstage_item?(item) && item.sell_in < 0
+          item.quality = 0 #2
+      end
+        end
+
+        def backstage_item?(item)
+          item.name == "Backstage passes to a TAFKAL80ETC concert"
+        end
+
+        def after_sell_in?(item)
+          item.sell_in < 0
+        end
+
+
+
+
+
   end
 
 
